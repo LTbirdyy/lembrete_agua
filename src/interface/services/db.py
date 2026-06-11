7da7cc7131693e8eb4fd738deca7d3ea59cd61cc
LTbirdyy <gabriel.fernando@sempreceub.com>
2026-06-11 16:22:31 -0300

    Atualização do readme + correções com flake8

--- src/interface/services/db.py
+++ src/interface/services/db.py
@@ -9,5 +9,4 @@ def get_supabase():
     url = os.getenv("SUPABASE_URL")
     key = os.getenv("SUPABASE_KEY")
 
-
     return create_client(url, key)
--- test_db.py
+++
@@ -1,7 +0,0 @@
-from src.interface.services.db import get_supabase
-
-supabase = get_supabase()
-
-resultado = supabase.table("historico").select("*").execute()
-
-print(resultado.data)
